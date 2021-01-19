c278c0ec961737314c697c84fd077d278caf78f7
NatielSanti <natielsanti@mail.ru>
2021-01-19 13:53:09 +0300

    third commit
    Signed-off-by: NatielSanti <natielsanti@mail.ru>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,8 @@
-import statictest.HWStaticTestFirst;
 
 public class Main {
     public static void main(String[] args) {
-//        LogicOperatorTest a = new LogicOperatorTest();
-//        OrderChild ch = new OrderChild();
-//        System.out.println("Hello World!");
-//        Object > Animal > Cat > HomeCat
-
-//        StaticTest st1 = new StaticTest();
-//        StaticTest st2 = new StaticTest();
-//        st1.method();
-//        st1.method();
-//        st2.method();
-
+        int a = 10;
+        int b = 15;
+        System.out.println(a + b);
     }
 }
--- src/main/java/statictest/StaticMain.java
+++ src/main/java/statictest/StaticMain.java
@@ -2,7 +2,7 @@ package statictest;
 
 public class StaticMain {
     public static void main(String[] args) {int a = 10;
-        System.out.println(a + HWStaticTestFirst.b); //Debug this. Answer should be == 15
+//        System.out.println(a + HWStaticTestFirst.b); //Debug this. Answer should be == 15
         System.out.println(a + HWStaticTestTwo.b); //Debug this. Answer should be == 15
     }
 }
