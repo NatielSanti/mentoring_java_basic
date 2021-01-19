5b02d042fa61fd3f0a8fa053ed0485079dc856d7
NatielSanti <natielsanti@mail.ru>
2021-01-19 16:20:33 +0300

    wqe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 15;
-        System.out.println(a + b);
+        int c = 30;
+        int d = 5;
+        System.out.println(a + b + c + d);
     }
 }
