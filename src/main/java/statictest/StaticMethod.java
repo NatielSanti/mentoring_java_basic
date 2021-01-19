b6e8172b9bfa82fcb705885523c1126a0f4136fd
NatielSanti <natielsanti@mail.ru>
2021-01-19 16:54:08 +0300

    wqe

--- src/main/java/statictest/StaticMethod.java
+++ src/main/java/statictest/StaticMethod.java
@@ -1,7 +1,15 @@
 package statictest;
 
 public class StaticMethod {
+    public int a = 10;
+    public static int b = 100;
+    public int c;
+
     public static void method(){
         System.out.println("Parent static method");
+    }
+
+    public void method2(){
+        System.out.println("Non static method");
     }
 }
