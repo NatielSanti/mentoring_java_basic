ce8d41a1ec131d5946e53b6fd38da1bf2d556c26
JI HOON PARK <pjhk1000@gmail.com>
2021-01-19 17:33:15 +0300

    rrr

--- src/main/java/statictest/HWStaticTestFirst.java
+++ src/main/java/statictest/HWStaticTestFirst.java
@@ -3,3 +3,23 @@ package statictest;
 public class HWStaticTestFirst {
     public int b = 5;
 }
+public class StaticMain {
+    public static void main(String[] args) {
+
+//        System.out.println(a + HWStaticTestFirst.b); //Debug this. Answer should be == 15
+//        System.out.println(a + HWStaticTestTwo.b); //Debug this. Answer should be == 15
+
+        StaticMethod t = new StaticMethod();
+
+ //       StaticMethod qwe = new StaticMethod();
+ //       qwe.c = 20;
+ //       StaticMethod.method();
+ //       t.method2();
+ //       System.out.println(t.c + StaticMethod.b);
+ //       System.out.println(qwe.c + StaticMethod.b);
+ //       StaticMethod.b = 200;
+ //       System.out.println(t.c + StaticMethod.b);
+ //       System.out.println(qwe.c + StaticMethod.b);
+
+    }
+}
